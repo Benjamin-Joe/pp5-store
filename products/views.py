2c0d08db410650e68b05a48483f7ce981884e444
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-05 23:11:05 +0000

    Create and add css to product_detail.html

--- products/views.py
+++ products/views.py
@@ -1,8 +1,7 @@
 "Products Views"
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
-
+from .models import Product
 
 def store(request):
     "View to return the all products shopping area"
@@ -15,3 +14,13 @@ def store(request):
         'all_products': all_products
     }
     return render(request, 'products/store.html', context)
+
+# Code below came from Code Institute
+def product_detail(request, product_id):
+    "View to return a requested products details"
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
+# End on taken code
