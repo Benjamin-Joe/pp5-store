5ea21b4db7e21498fc5405863d0a1c1b11debd38
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-05 20:55:39 +0000

    Add pagination ability to store.html

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 "Products Views"
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 
 def store(request):
     "View to return the all products shopping area"
     products = Product.objects.all()
+    p = Paginator(Product.objects.all(), 12)
+    page = request.GET.get('page')
+    all_products = p.get_page(page)
     context = {
         'products': products,
+        'all_products': all_products
     }
     return render(request, 'products/store.html', context)
