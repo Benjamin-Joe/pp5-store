24ef8ad12a9ce662a3965d5cdd8ac0f1e498e7f4
Benjamin-Joe <benjaminjoe43@gmail.com>
2022-02-06 13:12:48 +0000

    Add search functionality for title and description in navbar

--- products/views.py
+++ products/views.py
@@ -1,17 +1,30 @@
 "Products Views"
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Product
 
 def store(request):
     "View to return the all products shopping area"
+    query = None
     products = Product.objects.all()
     p = Paginator(Product.objects.all(), 12)
     page = request.GET.get('page')
     all_products = p.get_page(page)
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                return redirect(reverse('products'))
+
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            all_products = products.filter(queries)
+
     context = {
         'products': products,
-        'all_products': all_products
+        'all_products': all_products,
+        'search_term': query,
     }
     return render(request, 'products/store.html', context)
 
